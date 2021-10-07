5432d3506aab55171ed38959a9aef685ade3fe04
randall.erasmus <randallerasmus1@gmail.com>
2021-10-08 01:26:56 +0200

    commit test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello Github this is working");
+        System.out.println("Hello Github this is working and this is just a test");
     }
 }
