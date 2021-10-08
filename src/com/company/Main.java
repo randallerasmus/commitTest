f0bc0e339b84b75a1f3de7f4f5525f93e6c19dd6
randall.erasmus <randallerasmus1@gmail.com>
2021-10-08 03:47:07 +0200

    This is the changes - testing the git pull function

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Github this is working and this is just a test");
         System.out.println("Hello Github this is working and this is just a test");
+        System.out.println("Hello Github this is working and this is just a test");
     }
 }
