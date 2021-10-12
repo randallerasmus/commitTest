f916cbaaa9340188eba8bc13b3fedf3a57706084
randall.erasmus <randallerasmus1@gmail.com>
2021-10-12 17:33:48 +0200

    This is the changes - git merge

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,5 +7,6 @@ public class Main {
         System.out.println("Hello Github this is working and this is just a test");
 //        System.out.println("Hello Github this is working and this is just a test");
 //        System.out.println("Hello Github this is working and this is just a test");
+//        System.out.println("Hello Github this is working and this is just a test");
     }
 }
